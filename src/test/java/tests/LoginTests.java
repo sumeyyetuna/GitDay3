5ca55ab3c8a41538971db48ceebe631f1378f9e0
sumeyyetuna$ <sumeyyetunaa16@gmail.com>
2019-02-04 12:14:25 -0800

    Add negative test

--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -33,6 +33,15 @@ public class LoginTests {
 
     }
     @Test
+    public void negativeLoginTest(){
+        driver.get("http://secure.smartbearsoftware.com/samples/testcomplete12/WebOrders/login.aspx");
+        driver.findElement(By.id("ctl00_MainContent_username")).sendKeys("Tester2");
+        driver.findElement(By.id("ctl00_MainContent_password")).sendKeys("test2"+ Keys.ENTER);
+        String errorMsg=driver.findElement(By.id("ctl00_MainContent_status")).getText();
+        Assert.assertEquals(errorMsg,"Invalid Login or Password.");
+
+    }
+    @Test
     public void logOutTest(){
         driver.get("http://secure.smartbearsoftware.com/samples/testcomplete12/WebOrders/login.aspx");
         driver.findElement(By.id("ctl00_MainContent_username")).sendKeys("Tester");
