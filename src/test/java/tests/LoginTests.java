828321e3f16e6b7b762d44761dda2adbc5cbbe3e
sumeyyetuna$ <sumeyyetunaa16@gmail.com>
2019-02-04 11:46:28 -0800

    Commit 3.Ass logout test

--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -32,6 +32,14 @@ public class LoginTests {
         Assert.assertEquals(driver.getTitle(),"Web Orders");
 
     }
+    @Test
+    public void logOutTest(){
+        driver.get("http://secure.smartbearsoftware.com/samples/testcomplete12/WebOrders/login.aspx");
+        driver.findElement(By.id("ctl00_MainContent_username")).sendKeys("Tester");
+        driver.findElement(By.id("ctl00_MainContent_password")).sendKeys("test"+ Keys.ENTER);
+        driver.findElement(By.id("ctl00_logout")).click();
+        Assert.assertEquals(driver.getTitle(),"Web Orders Login");
+    }
     @AfterMethod
     public void cleaUp(){
         driver.close();
