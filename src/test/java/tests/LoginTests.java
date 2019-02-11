589c8235251d252eee120de4e4be948ffbd90a20
sumeyyetuna$ <sumeyyetunaa16@gmail.com>
2019-02-11 07:43:30 -0800

    Add LoginPage and comment

--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -12,7 +12,8 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
-
+//LoginTests are maintained here,
+//without Page object approach
 public class LoginTests {
 
     WebDriver driver;
