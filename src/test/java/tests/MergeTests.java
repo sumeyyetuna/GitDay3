8a31d325a18b15e330543a6f5baaa6d6b8845c52
sumeyyetuna$ <sumeyyetunaa16@gmail.com>
2019-02-18 11:02:30 -0800

    add mergetest

--- src/test/java/tests/MergeTests.java
+++ src/test/java/tests/MergeTests.java
@@ -1,4 +1,10 @@
 package tests;
 
+import org.testng.annotations.Test;
+
 public class MergeTests {
+    @Test
+    public void mergeTest(){
+        System.out.println("Testing the merge");
+    }
 }
